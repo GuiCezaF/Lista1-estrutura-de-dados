db3245bed97c0d11f73ae709bb88aea05032a788
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2024-02-22 00:01:03 +0000

    [Estrutura de dados] - Exercício 1 da lista corrigido

--- ex1.java
+++ ex1.java
@@ -1,16 +1,29 @@
+
+/*
+Considere um “produtório” como o resultado da multiplicação de um conjunto de
+números entre eles mesmos. Crie uma função que recebe como parâmetro de entrada
+um vetor de números inteiros e retorna o resultado da multiplicação de todos os
+números positivos entre s
+ */
+
 public class ex1 {
 
   static int Multiplica(int valores[]){
     int mult = 1;
+    
     for (int i = 0; i < valores.length; i++){
-      mult *= valores[i];
+      if (valores[i] >= 0){
+        mult *= valores[i];
+      }
     }
     return mult;
   }
   public static void main(String[] args) {
     int[] vetor = new int[]{
-      5,
-      3
+      -10,
+      20,
+      -8,
+      2,
     };
     System.out.println(Multiplica(vetor));
   }
